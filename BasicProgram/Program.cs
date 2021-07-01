25df3f0cc55d30551d19ea63630c26d65eb3953d
shalini <shalini.v@tvsnext.io>
2021-07-01 11:19:43 +0530

    [shalini]finding leap year

---
+++ BasicProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year%4==0)&&(year%100!=0)||(year%400==0))
+            {
+                Console.WriteLine("{0} is leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year ", year);
+            }
+        }
+    }
+}
